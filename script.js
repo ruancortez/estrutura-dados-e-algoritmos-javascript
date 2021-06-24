41db8b4abbe08b8708aa4ff57d8aa9d05d4f92ff
ruancortez <ruanvictorcortez@gmail.com>
2021-06-24 20:00:12 -0300

    1.9

--- script.js
+++ script.js
@@ -131,3 +131,10 @@ Operador lógico         |       Descrição
     ||                              Ou
     !                               Negação
 */
+// A linguagem JavaScript também tem suporte para operadores bit a bit (bitwise):
+console.log('5 & 1:', (5 & 1));
+console.log('5 | 1:', (5 | 1));
+console.log('~ 5:', (~5));
+console.log('5 ^ 1:', (5 ^ 1));
+console.log('5 << 1:', (5 << 1));
+console.log('5 >> 1:', (5 >> 1));
